815f4e727c60ecded8722b37a1ece47fb7152c66
markhwhiteii <markhwhiteii@gmail.com>
2019-09-10 09:29:25 -0400

    export, add return, need to still add description and examples

--- R/twipc_means.R
+++ R/twipc_means.R
@@ -1,5 +1,7 @@
 #' Simulation-Based Power Analysis for Two-Way Interaction Using Mean Patterns
 #' 
+#' @description 
+#' 
 #' @param a1b1 Mean in cell A1, B1
 #' @param a1b2 Mean in cell A1, B2
 #' @param a2b1 Mean in cell A2, B1
@@ -9,6 +11,13 @@
 #' @param by The incrementing size from start to end
 #' @param alpha The alpha level for determining significance
 #' @param reps The number of times to repeat the simulations
+#' 
+#' @return A data.frame containing columns indicating sample size and power
+#'   at that sample size.
+#' 
+#' @examples
+#' 
+#' @export
 twipc_means <- function(a1b1, a1b2, a2b1, a2b2, 
                         start, end, by, alpha, reps) {
   out <- lapply(
